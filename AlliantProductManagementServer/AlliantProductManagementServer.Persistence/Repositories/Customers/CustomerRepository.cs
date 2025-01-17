38a5c39c899bf6fc7b385ef2b131f2ea12dbbcdb
Alam Mena Beato <amenabeato@gmail.com>
2025-01-17 18:12:34 -0400

    Adding customer repository, and test for it

---
+++ AlliantProductManagementServer/AlliantProductManagementServer.Persistence/Repositories/Customers/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using AlliantProductManagementServer.Domain.Entities.Customers;
+using AlliantProductManagementServer.Domain.Repositories.Core;
+using AlliantProductManagementServer.Domain.Repositories.Customers;
+using AlliantProductManagementServer.Persistence.Contexts;
+using AlliantProductManagementServer.Persistence.Repositories.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlliantProductManagementServer.Persistence.Repositories.Customers
+{
+    public class CustomerRepository(ApplicationContext dbContext) : Repository<Customer>(dbContext), ICustomerRepository
+    {
+    }
+}
---
+++ AlliantProductManagementServer/AlliantProductManagementServer.Tests/Persitence/Repositories/Customers/CustomersRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using AlliantProductManagementServer.Domain.Entities.Customers;
+using AlliantProductManagementServer.Persistence.Contexts;
+using AlliantProductManagementServer.Persistence.Repositories.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlliantProductManagementServer.Tests.Persitence.Repositories.Customers
+{
+    public class CustomersRepositoryTests
+    {
+        [Fact]
+        public async Task TestCustomerCreation()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("TestDatabase")
+                .Options;
+
+            await using var context = new ApplicationContext(options);
+
+            var customerRepository = new CustomerRepository(context);
+
+            var customer = await customerRepository.AddAsync(new Customer
+            {
+                Name = "Alam",
+                LastName = "Mena",
+                Phone = "829-794-8271",
+                Identification = "401-1389763-6"
+            });
+
+            var entityCount = await context.Customers.CountAsync();
+
+            Assert.Equal(1, entityCount);
+        }
+    }
+}
